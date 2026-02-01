341d14a768be0f46cdb79ee6187a58d4d23743b4
אלעזר גרשי <emg4706@gmail.com>
2026-02-01 17:42:32 +0200

    io files phase 2

---
+++ files projects/io_filesproject.py
@@ -0,0 +1,81 @@
+import random
+
+def input_integrity():
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            if num > 500 or num < 0:
+                print("out of range")
+            else:
+                return num
+        except ValueError:
+            print("Invalid input")
+
+def is_right_number(SECRET_NUMBER,num):
+    if num > SECRET_NUMBER:
+        print("The number is higher than the secret number!")
+        return False
+    if num < SECRET_NUMBER:
+        print("The number is lower than the secret number!")
+        return False
+    if num == SECRET_NUMBER:
+        print("Right guess!")
+    return True
+
+def valid_user(username,password):
+    with open("users.txt","r",encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        listi = line.split(":")
+        if listi[0] == username and listi[1] == password:
+            return True
+    raise "Invalid username or password"
+
+
+
+username = input("Enter your name: ")
+password = input("Enter your password: ")
+valid_user(username,password)
+
+with open("guesses.txt","r",encoding="utf-8") as f:
+    record = f.readline().rstrip("\n")
+if record:
+    print(f"Win record: {record}")
+
+
+SECRET_NUMBER = random.randint(0,500)
+win = False
+guesses = 0
+
+while not win:
+    num = input_integrity()
+    win = is_right_number(SECRET_NUMBER,num)
+    guesses += 1
+
+if guesses < int(record):
+    with open("guesses.txt", "w", encoding="utf-8") as f:
+        f.write(str(guesses))
+    print("saved new record")
+print()
+print(f"Secret number: {SECRET_NUMBER}")
+print(f"number of guesses: {guesses}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
